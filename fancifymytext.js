2e23e6cf9b56db55b316175784baa7cda5fe2485
NodnarbOK <brandonkue12@gmail.com>
2024-02-22 20:38:45 -0800

    Added Moo
    I added the Moo function to the JavaScript file.

--- fancifymytext.js
+++ fancifymytext.js
@@ -17,3 +17,23 @@ function applyStyles() {
     textInput.style.color = ""; // Remove color style
     textInput.style.textDecoration = ""; // Remove the underline
   }
+
+  function makeMoo() {
+    var textInput = document.getElementById("textInput");
+    var text = textInput.value;
+    // Add "-Moo" to the last word of each sentence
+    var sentences = text.split(".") // Split the sentences
+    alert(sentences)
+    // Add -Moo at the end
+    for (var i = 0; i < sentences.length; i++) {
+        // Check if the sentence is a sentence or a blank
+        alert(sentences[i].length);
+        if (sentences[i].length > 0) { 
+            sentences[i] = sentences[i] + "-Moo"
+        }
+    }
+    // Join sentences with ". "
+    text = sentences.join(".");
+    // Uppercase the text
+    textInput.value = text.toUpperCase();
+  }
