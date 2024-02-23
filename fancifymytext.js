573316be4a63748dba4503339c57f09c5d83a070
NodnarbOK <brandonkue12@gmail.com>
2024-02-22 20:11:13 -0800

    Radio Button Functionality
    Added the fancify and boring functions to the buttons as well as took the fieldset out of the form since the form kept resetting everything.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,3 +1,19 @@
 function biggerText() {
     document.getElementById("textInput").style.fontSize = "24pt";
 }
+
+function applyStyles() {
+    var textInput = document.getElementById("textInput");
+    textInput.style.fontWeight = "bold"; // Set font weight to bold
+    if (document.getElementById("fancy").checked) {
+      textInput.style.color = "blue"; // Set color to blue
+      textInput.style.textDecoration = "underline"; // Underline the text
+    }
+}
+  
+  function removeStyles() {
+    var textInput = document.getElementById("textInput");
+    textInput.style.fontFamily = ""; // Remove font family style
+    textInput.style.color = ""; // Remove color style
+    textInput.style.textDecoration = ""; // Remove the underline
+  }
